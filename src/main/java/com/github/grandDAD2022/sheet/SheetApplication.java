9c802617ca1bda8c7ab38ac1d2684ff64ecc4875
Xerz <xerz@xerz.one>
2022-02-03 12:54:13 +0100

    Implementación inicial de Spring

---
+++ src/main/java/com/github/grandDAD2022/sheet/SheetApplication.java
@@ -0,0 +1,13 @@
+package com.github.grandDAD2022.sheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/grandDAD2022/sheet/SheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grandDAD2022.sheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
